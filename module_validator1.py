e2d2058863cbcc7e9008a5f26d6133a8d4fd2cbb
Homa Salami <salamih1881@gmail.com>
2025-10-27 14:04:42 +0330

    Add files via upload

--- module_validator1.py
+++ module_validator1.py
@@ -1,26 +1,41 @@
 import re
+import pickle
+from datetime import datetime
 
-total = 0
 
-def name_vallidator(name):
-    if re.match(r"^[a-zA-Z\s]$",name):
-        return True
-    else:
-        return False
+def name_validator(name):
+    if not re.match(r"^[a-zA-Z\s]+$",name):
+        raise NameError("Try Name Again!")
     
 def brand_validator(brand):
-    if re.match(r"^[a-zA-Z\s]{3,30}$",brand):
-        return True
-    else:
-        return False
+    if not re.match(r"^[a-zA-Z\s]{3,30}$",brand):
+        raise NameError("Try Brand Again!")
+
+def quantity_validator(quantity):
+    if not (quantity > 0 and type(quantity) == int):
+        raise NameError("Try Quantity Again!")
+
+def price_validator(price):
+    if not (price > 0 and type(price) == int):
+        raise NameError("Try Price Again!")
+    
+def exp_validator(exp_date):
+    expire_date = datetime.strptime(exp_date, "%Y-%m-%d").date()
+    if not expire_date >= datetime.today().date():
+        raise NameError ("Try EXP Date Again!")    
 
 
-def save_to_file(product_list)
-    pass
+def save_to_file(product_list):
+    shopping_file = open("shopping_list" , "wb")
+    pickle.dump(product_list , shopping_file) 
+    shopping_file.close()   
 
-def total() 
-    pass
+def total_product(product_list):
+    total = 0
+    for item in product_list:
+        total += item["quantity"]*item["price"]
+    return total
 
-def show_product(product_list):
-    for product in product_list:
-        pass
+#def show_product(product_list):
+    #for product in product_list:
+        #pass
--- super_market_app1.py
+++ super_market_app1.py
@@ -1,57 +1,75 @@
+from module_validator1 import *
 from tkinter import *
 from tkinter import messagebox
+from datetime import datetime
+
 product_list = []
 
 def save():
     try:
-        # if data validation
-        product = {"name": name.get(), "brand":brand.get(), "quantity":quantity.get() ,"price":price.get() ,"exp_date":exp_date.get()}
-        product.append(product)
-        messagebox.showinfo("Save", "Product Saved")
-        name.set("")
+        # get every parameter from user,and check if data validation based on module file
+        name_validator(name.get())
+        brand_validator(brand.get())
+        quantity_validator(quantity.get())
+        price_validator(price.get())
+        exp_validator(exp_date.get())
 
+        # check if data validator was +, then put in dict and add to product_list
+        product = {"name": name.get(),
+                   "brand":brand.get(),
+                   "quantity":quantity.get(),
+                   "price":price.get(),
+                   "exp_date":exp_date.get()
+                   }
+        
+        product_list.append(product)
+        save_to_file(product_list)
+        messagebox.showinfo("Saved!", "Product has joined the list Happily :)")
+        name.set("")
     except Exception as e:
-        pass
+        messagebox.showerror("OoPS! Save Error!!!", f"Error: {e}")
+
 
 def total():
     try:
-        pass
+        #todo total
+        total_product(product_list)
+        messagebox.showinfo("Total Price", f"Total Price is : {total_product(product_list)} TOMAN ")
     except Exception as e:
-        pass
+        messagebox.showinfo("Oops! Error!!!", f"Error: {e}")
+
 
 
 window = Tk()
-window.title("kharid")
-window.geometry("300x300")
+window.title("Shopping")
+window.geometry("320x320")
 
 #name
-Label(window,text="Name").place(x=20,y=20)
+Label(window,text="Name").place(x=30,y=30)
 name = StringVar()
-Entry(window,textvariable = name).place(x=100,y=20)
+Entry(window,textvariable = name).place(x=120,y=30)
 
 #brand
-
-Label(window,text="Brand").place(x=20,y=60)
+Label(window,text="Brand").place(x=30,y=70)
 brand = StringVar()
-Entry(window,textvariable = brand).place(x=100,y=60)
+Entry(window,textvariable = brand).place(x=120,y=70)
 
 #quantity
-
-Label(window,text="Quantity").place(x=20,y=100)
+Label(window,text="Quantity").place(x=30,y=110)
 quantity = IntVar()
-Entry(window,textvariable=quantity).place(x=100,y=100)
+Entry(window,textvariable=quantity).place(x=120,y=110)
 
 #price
-Label(window,text="Price").place(x=20,y=140)
+Label(window,text="Price").place(x=30,y=150)
 price = IntVar()
-Entry(window,textvariable=price).place(x=100,y=140)
+Entry(window,textvariable=price).place(x=120,y=150)
 
 #expire_date
-Label(window,text="Exp. Date").place(x=20,y=180)
+Label(window,text="Expiration Date\n YYYY-MM-DD").place(x=30,y=190)
 exp_date = StringVar()
-Entry(window,textvariable=exp_date).place(x=100,y=180)
+Entry(window,textvariable=exp_date).place(x=120,y=190)
 
 
-Button(window,text="Save", command=save).place(x=40,y=240,width=80)
-Button(window,text="Total",command=total).place(x=150,y=240,width = 80)
+Button(window,text="Save", command=save).place(x=30,y=255,width=100)
+Button(window,text="Total",command=total).place(x=145,y=255,width =100)
 window.mainloop()
